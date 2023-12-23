bb37304489176c4123f1935b9b44c7d87eb7528a
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2023-12-23 11:17:18 +0200

    fin

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,6 @@
 import {
   persistStore,
   persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { configureStore } from '@reduxjs/toolkit';
@@ -25,12 +19,6 @@ export const store = configureStore({
     contacts: persistedContactReducer,
     filter: filterReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
 
 export const persistor = persistStore(store);
